bd3d3c9fc4e792984b2082f5c8c21f05901f460d
Wyn <wynpennant@gmail.com>
2017-05-09 22:19:03 +0000

    plans and user

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
- # encoding: UTF-8
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170507141046) do
+ActiveRecord::Schema.define(version: 20170509221009) do
 
   create_table "contacts", force: true do |t|
     t.string "name"
